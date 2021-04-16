a7f5c67f782a1cd9b9c436c134d1efe75f0f092e
Lucas <lucasdamiangebhardt@gmail.com>
2021-04-16 19:34:48 -0300

    clase 4

---
+++ Trabajo Práctico 2/clase 4.py
@@ -0,0 +1,52 @@
+#! FUNCION -->  0 resultados, 1 resultado, muchos resultados
+
+
+# def suma(num1, num2):
+#     return num1 + num2
+
+# numero1 = 3
+# numero2 = 7
+
+# suma(numero1, numero2)
+
+
+# def suma_resta(num1, num2):
+#     return num1 + num2, num1 - num2
+
+# numero1 = 3
+# numero2 = 7
+# total, diferencia = suma_resta(numero1, numero2)
+
+# print(total, diferencia)
+
+# def listado(conjunto_datos):
+#     for persona in conjunto_datos:
+#         print(persona)
+
+# datos = ['ana', 'juan', 'julieta', 'mariano']
+
+# listado(datos)
+
+# print()
+
+# datos.append('pedro')
+# datos.append('maria')
+# datos.append('esteban')
+
+# listado(datos)
+
+
+
+def suma(num1, num2, total):
+    total = num1 + num2
+    return = total
+
+numero1 = 3
+numero2 = 7
+total = 0
+suma(numero1, numero2, total)
+
+print('el resultado de la suma es', total)
+
+
+
---
+++ Trabajo Práctico 2/mis_funciones.py
@@ -0,0 +1,9 @@
+
+#! variables primitivas inmutables numericas, string, boolean
+def suma(num1, num2, total):
+    total = num1 + num2
+
+
+#! cualquiera que no sea primitiva mutables
+def agregar(lista, valor):
+    lista[0] = valor
