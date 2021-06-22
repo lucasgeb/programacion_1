bc089eee501a64c5bc6f7c69d1b1875a350831c3
Lucas <lucasdamiangebhardt@gmail.com>
2021-06-22 00:06:10 -0300

    funcionando

--- weather_api/weather_API.py
+++ weather_api/weather_API.py
@@ -9,48 +9,8 @@ def obtener_clima(ruta):
     else:
         print('Error')
 
-def pronostico_por_lat_lon(lat, lon):
 
-    url_lat_lon = 'https://api.openweathermap.org/data/2.5/onecall?'
-    api_key = '6a19af64431d36030095df61d4e2abdd'
-    latitud = lat
-    longitud = lon
-    units = "metric"
-
-    parametros = {"lat":latitud, "lon":longitud, "units": units, "appid":api_key}
-
-    response = requests.get(url_lat_lon, params=parametros)
-
-    data = json.loads(response.content)
-
-    print("el pronóstico para la próxima hora es: ")   
-    print('Temperatura:', str(data['hourly'][0]['temp']))
-    print('Sensación térmica:', str(data['hourly'][0]['feels_like']))
-    print('Humedad:', str(data['hourly'][0]['humidity']))
-
-def clima_por_lat_lon(lat, lon):
-
-    url_lat_lon = 'https://api.openweathermap.org/data/2.5/onecall?'
-    api_key = '6a19af64431d36030095df61d4e2abdd'
-    latitud = lat
-    longitud = lon
-    units = "metric"
-
-    parametros = {"lat":latitud, "lon":longitud, "units": units, "appid":api_key}
-
-    response = requests.get(url_lat_lon, params=parametros)
-
-    data = json.loads(response.content)
-
-    print("el clima actual es: ")   
-    print('Temperatura:', data['current']['temp'])
-    print('Sensación térmica:', data['current']['feels_like'])
-    print('Humedad:', data['current']['humidity'])
-
-
-
-
-def clima_por_ciudad(ciudad):
+def clima_ciudad(ciudad):
     url_ciudad = 'https://api.openweathermap.org/data/2.5/weather?'
     api_key = '6a19af64431d36030095df61d4e2abdd'
     ciudad = ciudad
@@ -60,27 +20,119 @@ def clima_por_ciudad(ciudad):
 
     response = requests.get(url_ciudad, params=parametros)
 
-    data = json.loads(response.content)
+    data_clima_ciudad = json.loads(response.content)
+    info = {
+        'Temperatura' : data_clima_ciudad['main']['temp'],
+        'Humedad' : data_clima_ciudad['main']['humidity'],
+        'Sensación Térmica' : data_clima_ciudad['main']['feels_like']
+    }
+    return info
 
+
+def clima_coord(lat, lon):
+
+    url_lat_lon = 'https://api.openweathermap.org/data/2.5/weather?'
+    api_key = '6a19af64431d36030095df61d4e2abdd'
+    latitud = lat
+    longitud = lon
+    units = "metric"
+
+    parametros = {"lat":latitud, "lon":longitud, "units": units, "appid":api_key}
+
+    response = requests.get(url_lat_lon, params=parametros)
+
+    data_clima_coord = json.loads(response.content)
+    info = {
+        'Temperatura' : data_clima_coord['main']['temp'],
+        'Humedad' : data_clima_coord['main']['humidity'],
+        'Sensación Térmica' : data_clima_coord['main']['feels_like']
+    }
+    return info
+
+
+
+
+def pronostico_ciudad(ciudad):
+    url_ciudad = 'https://api.openweathermap.org/data/2.5/forecast?'
+    api_key = '6a19af64431d36030095df61d4e2abdd'
+    ciudad = ciudad
+    unidades = "metric"
+    exclude = "current,minutely,hourly,alerts"
+
+    parametros = {"q":ciudad, "units": unidades, "exclude": exclude, "appid":api_key}
+
+    response = requests.get(url_ciudad, params=parametros)
+
+    data_pronostico_ciudad = json.loads(response.content)
+    for item in data_pronostico_ciudad['list']:
+        info = {
+        'Fecha' : str(item['dt_txt']),
+        'Temperatura' : str(item['main']['temp']),
+        'Sensación térmica' : str(item['main']['feels_like']),
+        'Presión' : str(item['main']['pressure']),
+        'Humedad' : str(item['main']['humidity']),
+            }
+    print(info)
+    print()
+   
+
+
+def pronostico_coord(lat, lon):
+
+    url_lat_lon = 'https://api.openweathermap.org/data/2.5/forecast?'
+    api_key = '6a19af64431d36030095df61d4e2abdd'
+    latitud = lat
+    longitud = lon
+    units = "metric"
     
-    print('Latitud:', data['coord']['lat'])
-    print('Longitud', data['coord']['lon'])
-    print('Temperatura:', data['main']['temp'])
-    print('Sensación térmica:', data['main']['feels_like'])
-    print('Temperatura mínima:', data['main']['temp_min'])
-    print('Temperatura máxima:', data['main']['temp_max'])
-    print('Humedad:', data['main']['humidity'])
+
+    parametros = {"lat":latitud, "lon":longitud, "units": units, "appid":api_key}
+
+    response = requests.get(url_lat_lon, params=parametros)
+
+    data_pronostico_coord = json.loads(response.content)
     
+    for item in data_pronostico_coord['list']:
+        info = {
+        'Fecha' : item['dt_txt'],
+        'Temperatura' : item['main']['temp'],
+        'Sensación térmica' : item['main']['feels_like'],
+        'Presión' : item['main']['pressure'],
+        'Humedad' : item['main']['humidity'],
+            },
+        
+    print(info)
     
 
-  
-ciudad = input("ingrese la ciudad para la búsqueda: ")
-data_clima_ciudad = clima_por_ciudad(ciudad)
 
-print(data_clima_ciudad)
 
-lat= input("ingrese latitud: ")
-lon = input("ingrese la longitud: ")
-data_lat_lon = clima_por_lat_lon(lat, lon)
 
-print(data_lat_lon)
+menu=True
+while menu:
+    print("""
+    1.Consultar el clima actual buscando por ciudad
+    2.Consultar el clima actual buscando por coordenadas
+    3.consultar el pronostico buscando por ciudad
+    4.Consultar el pronostico buscando por coordenadas
+    5.Terminar
+    """)
+    menu=input("¿qué desea consultar?")
+    if menu=="1":
+        ciudad = input("ingrese la ciudad para la búsqueda: ")
+        print(clima_ciudad(ciudad))
+    elif menu=="2":
+        lat = input("ingrese latitud: ")
+        lon = input("ingre la longitud: ")
+        print(clima_coord(lat,lon))
+    elif menu=="3":
+        ciudad = input("ingrese la ciudad para la búsqueda: ")
+        print(pronostico_ciudad(ciudad))
+    elif menu=="4":    
+        lat = input("ingrese latitud: ")
+        lon = input("ingre la longitud: ")
+        print(pronostico_coord(lat,lon))
+    elif menu=="5":
+         print("Chau!") 
+         menu = None    
+    else:
+       print("La opción ingresada no es correcta")
